68379a8348e7bc97e80c79183e05496e566bfd66
Ignacio Rey <ignacior95@gmail.com>
2019-11-01 16:40:00 +0100

    Final version (hopefully)

--- smart_wedding.sol
+++ smart_wedding.sol
@@ -3,26 +3,19 @@ pragma solidity >0.5.10;
 
 contract SmartWedding{
     
-    // if you are testing the code set this value to true
-    // when areYouTesting = true, validTime() always return true
-    bool private areYouTesting = true;
-
-    string public marriageStatus;
-    uint public weddingTime;
-
     struct Spouse{
         address _address;
         string _firstName;
         string _lastName;
-        bool _cancelWedding;
+        bool _cancelWedding; //In case one of the spouses changes his/her mind before the wedding
     }
     
-    // constant struct types is not implemented yet.
-    // So we cannot use const spouse1 here
-    Spouse private spouse1;
+    Spouse private spouse1; //later we give them values, private for security reasons
     Spouse private spouse2;
-    bool guestObjected = false;
+    bool guestObjected = false; 
     bool objectingMajority = false; 
+    string public marriageStatus;//In this string we register the state of the wedding after this, e.g. married, cancelled...
+    uint public weddingTime;//Time the ceremony starts
     
 /* The code regarding timestamp to Date and Time conversion is taken from the Library BokkyPooBahsDateTimeLibrary found on Github via
  https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary/blob/53a99d2ca81270bcb4047c2ba342ad45e0fa17fd/contracts/BokkyPooBahsDateTimeLibrary.sol*/
@@ -79,49 +72,34 @@ contract SmartWedding{
         bool _objection; // does the guest object to this marriage
         bool _attendance;// does the guest accept to attend to the ceremony
         address _addressG;
-        bool _vote;
+        bool _vote;//In case of objection the guests have to vote if they agree to it or not
     }
     
     Guest[] public guestList;
     uint k;
     uint h = 0 ;
-    uint count = 0;
+    uint public count = 0; //this variable and the next are used to count the votes in case of objection
     uint validTotalCount = 0;
-    uint votingLimit;
+    uint votingLimit; //time limit to vote
     
+    //The following modifier is to set time windows for each function more easily
     modifier validTime(uint _timeFrom, uint _timeUntil, string memory _errorMessage){
-        if(areYouTesting == false){
             require(now >= _timeFrom && now < _timeUntil, _errorMessage);
-        }
-        _;
+            _;
     }
 
+    //Here we define our spouses
     constructor() public{
         spouse1 = Spouse(address(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c),"Romeo","Montague",false);
         spouse2 = Spouse(address(0x111122223333444455556666777788889999aAaa),"Juliet","Capulet",false);
         marriageStatus = "Not married yet";
-        weddingTime = 1590494400; //weddingTime set to 05/26/2020 at 12:00
+        weddingTime = 1572631200; //weddingTime set to 05/26/2020 at 12:00
     }   
     
-
-    function getSpouses() public view returns (string memory) {
-        return string(abi.encodePacked("Spouse 1:=>","address:",
-            "a valid address"," name:",spouse1._firstName," ",spouse1._lastName,"\n",
-            "Spouse 2:=>","address:",
-            "a valid address"," name:",spouse2._firstName," ",spouse2._lastName));
-    }
     
     modifier onlySpouse(){
         require(msg.sender == spouse1._address || msg.sender == spouse2._address, "Only spouses can do this action!");
         _;
-    }
-    
-    function addGuest(string memory _firstName, string memory _lastName, address _address) public onlySpouse
-    validTime(now, weddingTime-259200, "Cannot add guests less than 3 days before the wedding") returns (bool){
-        // check the ceremony date, if after the date disallow this function
-        // default _acceptance and _objection values are false
-        guestList.push(Guest(_firstName, _lastName, false, false, false, _address, false));
-        return true;
     }
     
     modifier onlyGuest(){
@@ -133,35 +111,15 @@ contract SmartWedding{
         _;                    
     }
     
-    function acceptInvitation() public onlyGuest 
-    validTime(now, weddingTime-259200, "You cannot accept the invitation now!") returns(string memory){
-        // check the ceremony date, if after the date disallow this function
-        guestList[k]._acceptance = true;
-        return ("See you at the wedding");
-    }
-    
-    modifier onlyAcceptedGuest(){
+    modifier onlyGuestAccepted(){
         for (k=0; k<guestList.length; k++) {
             if (guestList[k]._addressG == msg.sender)
                 break;
         }
-        require(guestList[k]._acceptance, "Only logged in guests can vote!");
+        require(guestList[k]._acceptance, "Only for guests that have accepted the invitation!");
         _;
     }
-
-    function attendCeremony() public onlyAcceptedGuest 
-    validTime(weddingTime-21600, weddingTime+7200, "Not open attending wedding") returns(string memory){ //6 hours before wedding, 2 hours after
-
-        guestList[k]._attendance = true;
-        return string("See you inside");
-    }
-
-    function toString(address x) internal pure returns (string memory) {
-        bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-        return string(b);
-    }
+    
     
     modifier onlyLoggedInGuest(){
         for (k=0; k<guestList.length; k++) {
@@ -172,27 +130,51 @@ contract SmartWedding{
         _;
     }
     
-    // A logged_in guest can vote 15 min before the ceremony and 15 min after started
+    //Function to invite guests to the wedding. Values of vote, acceptance, objection and attendance are initially 
+    //set to false as they are the ones who decide to object, wheter or not to assist, etc 
+    function addGuest(string memory _firstName, string memory _lastName, address _address) public onlySpouse
+    validTime(now, weddingTime, "Cannot add guests less than 3 days before the wedding") returns (bool){
+        guestList.push(Guest(_firstName, _lastName, false, false, false, _address, false));
+        return true;
+    }
+    
+    //Function to accept the wedding invitation
+    function acceptInvitation() public onlyGuest 
+    validTime(now, weddingTime, "You cannot accept the invitation now!") returns(string memory){
+        guestList[k]._acceptance = true;
+        return ("See you at the wedding");
+    }
+    
+    //Function to attend the wedding. The invitation has to be previously accepted
+    function attendCeremony() public onlyGuestAccepted 
+    validTime(now, weddingTime, "Not open attending wedding") returns(string memory){
+        guestList[k]._attendance = true;
+        return ("See you inside");
+    }
+    
+    //Function to object to the marriage, this will initiate a voting process. Guests need to attend the wedding to object
     function objectMarriage() public onlyLoggedInGuest 
-        validTime(weddingTime-900,weddingTime+900, "You can start to vote 15 min before the ceremony!"){
+        validTime(now,weddingTime, "You can start to vote 15 min before the ceremony!"){
         guestList[k]._objection = true;
         guestList[k]._vote = true;
         guestObjected = true;
         uint objectiontime = now;
-        votingLimit = objectiontime + 600;
+        votingLimit = objectiontime + 60;
     }
     
+    //In case of objection the other guests can agree with it through this function
     function agreeToObjection() public onlyLoggedInGuest returns (string memory){
         if (guestObjected == true){
             guestList[k]._vote = true;
         }
         else{
-            return ("You cannot agree to an objection that does not exist.");
+           return ("You cannot agree to an objection that does not exist.");    
         }
     }
     
+    //After the time for voting we can count the votes with this function. If majority agrees with the objection the wedding will be cancelled
     function calculateVoting() public onlyLoggedInGuest returns (string memory){
-        if (now > votingLimit){
+        if (now < votingLimit){
             for (uint i=0; i<guestList.length; i++)   
             {
                 if (guestList[i]._vote==true){
@@ -203,14 +185,16 @@ contract SmartWedding{
             }
             if (count> validTotalCount/2){
                 marriageStatus = "Objection accepted. Marriage is terminated.";
-                objectingMajority = true;
+                objectingMajority = true; 
             }
+            return ("Voting process finished");
         }
         else{
             return ("The voting is not yet over/There isn't any voting proccess");
         }
     }
     
+    //In case any of the spouses change his/her mind, he/she can cancel the wedding with this function before it happens
     function cancelMarriage() public onlySpouse
     validTime(now, weddingTime, "You can not cancel wedding after marriage") {
         if(msg.sender == spouse1._address) {
@@ -223,12 +207,11 @@ contract SmartWedding{
         
     }
     
-    function startCermony() public onlySpouse
-    validTime(weddingTime, weddingTime+900, "You can't start the cermony") { //cermony can start on weddingTime and until 15 minutes after
+    //after the wedding and if no objection accepted the spuses can change their status to happily married
+    function formalizeWedding() public onlySpouse
+    validTime(now, weddingTime+900, "You can't start the cermony") { //cermony can start on weddingTime and until 15 minutes after
         if (spouse1._cancelWedding == false && spouse2._cancelWedding == false && objectingMajority == false) {
-   
             marriageStatus = "Married";
         }  
-    
     }
 }
